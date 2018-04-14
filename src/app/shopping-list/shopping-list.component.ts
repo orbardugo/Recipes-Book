46303fc1e8d91f3bebcbb2f7fbfe526ee47165b7
orbardugo <orbardugo1@gmail.com>
2018-04-14 23:37:01 +0300

    init commit

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core"
+
+@Component({
+    selector: "app-shoppint-list",
+    templateUrl: "./shopping-list.component.html"
+})
+export class ShoppingListComponent{
+
+}
