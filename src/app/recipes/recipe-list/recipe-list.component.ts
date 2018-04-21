e62d4b472be3c2bd37907a5140ffc7ed57a2c546
orbardugo <orbardugo1@gmail.com>
2018-04-21 13:22:33 +0300

    adding some components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -6,5 +6,13 @@ import { Recipe } from '../recipe.model'
     templateUrl: "./recipe-list.component.html"
 })
 export class RecipeListComponent{
-    recipes: Recipe [] = [new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg')];
+    recipes: Recipe [] = [
+        new Recipe('A Test Recipe',
+        'This is simply a test',
+        'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg'),
+        new Recipe('A Test Recipe',
+        'This is simply a test',
+        'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg')
+    
+    ];
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,9 +1,18 @@
 import { Component } from "@angular/core"
+import { Ingredient } from "../shared/ingredient.model";
 
 @Component({
     selector: "app-shoppint-list",
     templateUrl: "./shopping-list.component.html"
 })
 export class ShoppingListComponent{
+    ingredients: Ingredient[] = [
+        new Ingredient("Apple",5),
+        new Ingredient("Tomatoes",10),
+    ];
 
+    constructor()
+    {
+
+    }
 }
